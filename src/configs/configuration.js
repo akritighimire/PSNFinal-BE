238b6f57de036112ff54aae9a89982bd99896bed
chandan thakur <95745383+devchandant225@users.noreply.github.com>
2025-03-07 15:46:28 +0545

    Update configuration.js

--- src/configs/configuration.js
+++ src/configs/configuration.js
@@ -1,7 +1,7 @@
 const configs = {
   mongodbURL:
  "mongodb+srv://akriti7ghimire:ihiHDiZwoaeFvur1@psnbe.ov1de.mongodb.net/?retryWrites=true&w=majority&appName=PSNBE",
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 4000,
 };
 
 module.exports = configs;
